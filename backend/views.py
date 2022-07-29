f531f2a417aeafb994ae6ddf370086cbf1c26f70
Akhadafi <khadafi.dev@gmail.com>
2022-07-29 20:57:50 +0700

    Class-Based-Views

--- backend/views.py
+++ backend/views.py
@@ -5,23 +5,8 @@ from django.views import View
 # Create your views here.
 
 
-def TaskList(request):
-    if request.method == "GET":
-        tasks = Task.objects.all().order_by("-updated")
-        context = {
-            "title": "Task",
-            "tasks": tasks,
-        }
-        return render(request, "backend/index.html", context)
-
-    if request.method == "POST":
-        task = Task.objects.create(body=request.POST.get("body"))
-        task.save()
-        return redirect("tasks")
-
-
-# class TaskList(View):
-#     def get(self, request):
+# def TaskList(request):
+#     if request.method == "GET":
 #         tasks = Task.objects.all().order_by("-updated")
 #         context = {
 #             "title": "Task",
@@ -29,30 +14,29 @@ def TaskList(request):
 #         }
 #         return render(request, "backend/index.html", context)
 
-#     def post(self, request):
+#     if request.method == "POST":
 #         task = Task.objects.create(body=request.POST.get("body"))
 #         task.save()
 #         return redirect("tasks")
 
 
-def TaskDetail(request, pk):
-    if request.method == "GET":
-        task = Task.objects.get(id=pk)
+class TaskList(View):
+    def get(self, request):
+        tasks = Task.objects.all().order_by("-updated")
         context = {
             "title": "Task",
-            "task": task,
+            "tasks": tasks,
         }
-        return render(request, "backend/task.html", context)
+        return render(request, "backend/index.html", context)
 
-    if request.method == "POST":
-        task = Task.objects.get(id=pk)
-        task.body = request.POST.get("body")
+    def post(self, request):
+        task = Task.objects.create(body=request.POST.get("body"))
         task.save()
         return redirect("tasks")
 
 
-# class TaskDetail(View):
-#     def get(self, request, pk):
+# def TaskDetail(request, pk):
+#     if request.method == "GET":
 #         task = Task.objects.get(id=pk)
 #         context = {
 #             "title": "Task",
@@ -60,37 +44,53 @@ def TaskDetail(request, pk):
 #         }
 #         return render(request, "backend/task.html", context)
 
-#     def post(self, request, pk):
+#     if request.method == "POST":
 #         task = Task.objects.get(id=pk)
 #         task.body = request.POST.get("body")
 #         task.save()
 #         return redirect("tasks")
 
 
-def TaskDelete(request, pk):
-    task = Task.objects.get(id=pk)
+class TaskDetail(View):
+    def get(self, request, pk):
+        task = Task.objects.get(id=pk)
+        context = {
+            "title": "Task",
+            "task": task,
+        }
+        return render(request, "backend/task.html", context)
 
-    if request.method == "POST":
-        task.delete()
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.body = request.POST.get("body")
+        task.save()
         return redirect("tasks")
 
-    context = {
-        "title": "Task",
-        "task": task,
-    }
-    return render(request, "backend/delete.html", context)
 
+# def TaskDelete(request, pk):
+#     task = Task.objects.get(id=pk)
 
-# class TaskDelete(View):
-#     def get(self, request, pk):
-#         task = Task.objects.get(id=pk)
-#         context = {
-#             "title": "Task",
-#             "task": task,
-#         }
-#         return render(request, "backend/delete.html", context)
-
-#     def post(self, request, pk):
-#         task = Task.objects.get(id=pk)
+#     if request.method == "POST":
 #         task.delete()
 #         return redirect("tasks")
+
+#     context = {
+#         "title": "Task",
+#         "task": task,
+#     }
+#     return render(request, "backend/delete.html", context)
+
+
+class TaskDelete(View):
+    def get(self, request, pk):
+        task = Task.objects.get(id=pk)
+        context = {
+            "title": "Task",
+            "task": task,
+        }
+        return render(request, "backend/delete.html", context)
+
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.delete()
+        return redirect("tasks")
